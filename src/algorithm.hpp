68b9767b0b2d4cadb247c29c2846d7ac7a1041ce
machinly <machinlyg@gmail.com>
2020-12-22 13:45:53 +0800

    Add some comments for count

--- src/algorithm.hpp
+++ src/algorithm.hpp
@@ -2,12 +2,15 @@
 
 namespace sltl
 {
+    /*
+     *  count: Count appearances of value in range
+     */
     template <class InputIterator, class T>
         typename std::iterator_traits<InputIterator>::difference_type
         count (InputIterator first, InputIterator last, const T& val) {
             typename std::iterator_traits<InputIterator>::difference_type ret = 0;
             while (first != last) {
-                if (*first == val){
+                if (*first == val) {
                     ret++;
                 }
                 first++;
